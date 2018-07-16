629ed4017f7e9bf68063c4f8b2c9826bd1479491
David Ma <davidma415@gmail.com>
2018-07-16 01:55:43 -0400

    Finished exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -1,16 +1,35 @@
 # initial step would be to just keep using this counter and assuming distance will not go greater than 10 000 kms.
 counter = 0
-options = ["walk","run", "go home"]
+energy = 100
+options = ["Walk","Run", "go home", "Eat", "Rest"]
+optLength = options.length
 while counter < 10000
-  puts "Would you like to walk or run?"
+  puts "==============================="
+  puts "Options: "
+  puts options [0..optLength]
+  puts "==============================="
+  puts "Your current energy is #{energy}"
+  puts "What would you like to do?"
   choice = gets.chomp.downcase
-  if choice == options[0] # if the user picks walk, distance will incr by 1
+  if choice == options[0].downcase # if the user picks walk, distance will incr by 1, they will also gain 1 energy
     counter += 1
-    puts "Distance from home is #{counter}km"
-  elsif choice == options[1] # if the user picks run, distance will incr by 5
+    energy += 1
+    puts "Distance from home is #{counter}km."
+  elsif choice == options[1].downcase && energy > 0 # if the user picks run, distance will incr by 5, as well as lose 5 energy
     counter += 5
-    puts "Distance from home is #{counter}km"
-  elsif choice == options[2]
+    energy -= 5
+    puts "Distance from home is #{counter}km."
+  elsif choice == options[1].downcase && energy == 0 # if the user picks run, distance will incr by 5, as well as lose 5 energy
+    puts "You cannot run as you have #{energy} energy. Please select another option, your current distance from home is #{counter}km."
+  elsif choice == options[3].downcase #option to eat, gain 10 energy
+    energy += 10
+    puts "You stop to eat some of your energy bar. Your energy is now #{energy}. Your current distance is #{counter}km."
+  elsif choice == options[4].downcase #option to rest, gain 30 energy
+    energy += 30
+    puts "You stop to take a quick breather. Your energy is now #{energy}. Your current distance is #{counter}km."
+  elsif (options.include? choice) == false # This will give an error if the selected option is not valid
+    puts "That is not a valid option. Select another. Your distance from home is #{counter}km."
+  elsif choice == options[2] # This will select go home and break the while loop
     break;
   end
 end
