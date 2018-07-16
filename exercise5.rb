39f8b1f2fc62f1c85504746f9aaa02e3b2c08be7
David Ma <davidma415@gmail.com>
2018-07-16 01:19:24 -0400

    Finished exercise 6.1

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,6 @@
 # initial step would be to just keep using this counter and assuming distance will not go greater than 10 000 kms.
 counter = 0
-options = ["walk","run"]
+options = ["walk","run", "go home"]
 while counter < 10000
   puts "Would you like to walk or run?"
   choice = gets.chomp.downcase
@@ -10,5 +10,7 @@ while counter < 10000
   elsif choice == options[1] # if the user picks run, distance will incr by 5
     counter += 5
     puts "Distance from home is #{counter}km"
+  elsif choice == options[2]
+    break;
   end
 end
